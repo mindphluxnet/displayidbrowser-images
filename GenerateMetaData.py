f274fc1445ef65d1e0e71858370bd0d7b3190d22
Richard Kämmerer <richard.kaemmerer@gmail.com>
2023-03-30 16:15:46 +0200

    Mark display_ids beyond a threshold

--- GenerateMetaData.py
+++ GenerateMetaData.py
@@ -3,7 +3,7 @@ import glob
 import mysql.connector
 import json
 db = mysql.connector.connect(
-    host="localhost", user="root", password="pwd", database="alpha_world")
+    host="192.168.178.10", user="root", password="pwd", database="alpha_world")
 cursor = db.cursor()
 
 path = os.path.join(os.path.dirname(__file__), 'images')
@@ -17,9 +17,12 @@ for name in glob.iglob('images/**/*.png', recursive=True):
         "SELECT entry, name, subname, scale FROM creature_template WHERE (display_id1 = %s OR display_id2 = %s OR display_id3 = %s OR display_id4 = %s)", (id, id, id, id))
     result = cursor.fetchall()
     used_by = []
+    color = 'success'
     for entry, name, subname, scale in result:
-        used_by.append({"entry": entry, "name": name, "subname": subname or '', "scale": scale or 1.0})
-    images.append({"id": id, "path": dir, "used": len(result) > 0, "used_by": used_by})
+        used_by.append({"entry": entry, "name": name, "subname": subname or '', "scale": scale or 1.0, "not_in_alpha": entry >= 5759})
+        if entry >= 5759:
+            color = 'warning'
+    images.append({"id": id, "path": dir, "used": len(result) > 0, "used_by": used_by, "color": color })
 
 if len(images) > 0:
     json.dump(images, open('images.json', 'w'), indent=4)
